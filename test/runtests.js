e01b7eb863d2bb915a6913eb6c830e603f65fac0
Michael Adair <madair@dmsolutions.ca>
2008-09-27 16:24:17 +0000

    re #2: dynamic loading through the <script> tag means a proxy script is no longer required. remove dependency on OpenLayers;

--- test/runtests.js
+++ test/runtests.js
@@ -1,16 +1,33 @@
-Proj4js.proxyScript = '/mapbuilder/proxy?url=';
-
+/* Loop through the test points and create a Proj object for each
+ */
 function runTests() {
-  //crossProj();
-  var xyEPSLN = 1.0e-2;
-  var llEPSLN = 1.0e-6;
-  var testTable = document.getElementById('testResult');
   for (var i=0; i < Proj4js.testPoints.length; ++i) {
-    var row = document.createElement('tr');
     var test = Proj4js.testPoints[i];
     var proj = new Proj4js.Proj(test.code);
-    //proj.getTestPoint();
-    
+    checkProjLoaded.call(proj, test);
+  }
+}
+
+/* a timer that waits for the readyToUse flag to be set.  This gets called in
+ * the context of the Proj4js.Proj object
+ */
+function checkProjLoaded(test) {
+    if (!this.readyToUse) {
+      window.setTimeout(Proj4js.bind(checkProjLoaded, this, test), 500);
+    } else {
+      showResults(this, test);
+    }
+  
+}
+
+/* a callback function to run the test for this test point since we are using
+ the dynamic load capabilities in the test page
+ */
+function showResults(proj, test) {
+  //var test = proj.testPoint;
+  var xyEPSLN = 1.0e-2;
+  var llEPSLN = 1.0e-6;
+    var row = document.createElement('tr');
     var td = document.createElement('td');
     td.innerHTML = test.code;
     row.appendChild(td);
@@ -18,6 +35,7 @@ function runTests() {
     td.innerHTML = proj.projName;
     row.appendChild(td);
     
+    //transform from lon/lat to projected x/y and cmopare 
     var xyResult = Proj4js.transform(Proj4js.WGS84, proj, new Proj4js.Point(test.ll));
     if (xyResult) {
       var deltaX = Math.abs(xyResult.x - test.xy[0]);
@@ -38,6 +56,7 @@ function runTests() {
       row.appendChild(td);
     }
     
+    //transform from map x/y to lon/lat and compare
     var llResult = Proj4js.transform(proj, Proj4js.WGS84, new Proj4js.Point(test.xy));
     if (llResult) {
       var deltaX = Math.abs(llResult.x - test.ll[0]);
@@ -58,43 +77,7 @@ function runTests() {
       row.appendChild(td);
     }
     
+    var testTable = document.getElementById('testResult');
     testTable.tBodies[0].appendChild(row);
-  }
-};
-    
-function crossProj() {
-  var prefGoogle = new Proj4js.Point(1484259.88, 6024072.12, 0.0);
-  var pref31285 = new Proj4js.Point(450055.70, 5262356.33, 0.0);
-  var prefWGS84 = new Proj4js.Point(13.3333333333, 47.5, 0.0);
-  var dest = new Proj4js.Proj('EPSG:31285');
-  var google = new Proj4js.Proj('google');
-  //var OLxform = new OpenLayers.Layer.SphericalMercator();
-  
-  var direct = Proj4js.transform(google, dest, prefGoogle.clone());//this should equal pref31285
-  var indirect = Proj4js.transform(dest, google, pref31285.clone());//this should equal prefGoogle
-  var WGS84Out = Proj4js.transform(dest, Proj4js.WGS84, pref31285.clone());
-  var destOut = Proj4js.transform(Proj4js.WGS84, dest, prefWGS84.clone());
-  var googleOut = Proj4js.transform(Proj4js.WGS84, google, prefWGS84.clone());
-  var cross1 = Proj4js.transform(dest, google, destOut.clone());  //this should equal googleOut
-  var cross2 = Proj4js.transform(google, dest, googleOut.clone());//this should equal destOut
-  return;
-}
-
-Proj4js.Proj.prototype.getBbox = function() {
-  //set AJAX options
-  var options = {
-    method: 'get',
-    asynchronous: false,          //need to wait until defs are loaded before proceeding
-    onSuccess: boundsLoaded.bind(this)
-  }
-  //load from web service via AJAX request
-  var url = Proj4js.proxyScript?Proj4js.proxyScript:'';
-  url += Proj4js.defsLookupService +'/' + this.srsAuth +'/'+ this.srsProjNumber + '/bbox';
-  new OpenLayers.Ajax.Request(url, options);
 };
 
-function boundsLoaded(transport) {
-  if (transport.responseText && transport.responseText.length>0 ) {
-    eval('this.bbox='+transport.responseText);
-  }
-}
