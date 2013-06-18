48072ffc269f1dd03cef1d2f1b4899db42e61af9
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-06-18 10:11:33 -0400

    remove async code

--- src/Proj.js
+++ src/Proj.js
@@ -20,7 +20,7 @@ Proj4js.Proj = Proj4js.Class({
    * Property: readyToUse
    * Flag to indicate if initialization is complete for this Proj object
    */
-  readyToUse: false,   
+  readyToUse: true,   
   
   /**
    * Property: title
@@ -143,140 +143,10 @@ Proj4js.Proj = Proj4js.Class({
           this.srsProjNumber = this.srsCode;
       }
       
-      this.loadProjDefinition();
+      this.parseDefs();
+      this.initTransforms();
   },
   
-/**
- * Function: loadProjDefinition
- *    Loads the coordinate system initialization string if required.
- *    Note that dynamic loading happens asynchronously so an application must 
- *    wait for the readyToUse property is set to true.
- *    To prevent dynamic loading, include the defs through a script tag in
- *    your application.
- *
- */
-    loadProjDefinition: function() {
-      //check in memory
-      if (Proj4js.defs[this.srsCode]) {
-        this.defsLoaded();
-        return;
-      }
-
-      //else check for def on the server
-      var url = Proj4js.getScriptLocation() + 'defs/' + this.srsAuth.toUpperCase() + this.srsProjNumber + '.js';
-      Proj4js.loadScript(url, 
-                Proj4js.bind(this.defsLoaded, this),
-                Proj4js.bind(this.loadFromService, this),
-                Proj4js.bind(this.checkDefsLoaded, this) );
-    },
-
-/**
- * Function: loadFromService
- *    Creates the REST URL for loading the definition from a web service and 
- *    loads it.
- *
- */
-    loadFromService: function() {
-      //else load from web service
-      var url = Proj4js.defsLookupService +'/' + this.srsAuth +'/'+ this.srsProjNumber + '/proj4js/';
-      Proj4js.loadScript(url, 
-            Proj4js.bind(this.defsLoaded, this),
-            Proj4js.bind(this.defsFailed, this),
-            Proj4js.bind(this.checkDefsLoaded, this) );
-    },
-
-/**
- * Function: defsLoaded
- * Continues the Proj object initilization once the def file is loaded
- *
- */
-    defsLoaded: function() {
-      this.parseDefs();
-      this.loadProjCode(this.projName);
-    },
-    
-/**
- * Function: checkDefsLoaded
- *    This is the loadCheck method to see if the def object exists
- *
- */
-    checkDefsLoaded: function() {
-      if (Proj4js.defs[this.srsCode]) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-
- /**
- * Function: defsFailed
- *    Report an error in loading the defs file, but continue on using WGS84
- *
- */
-   defsFailed: function() {
-      Proj4js.reportError('failed to load projection definition for: '+this.srsCode);
-      Proj4js.defs[this.srsCode] = Proj4js.defs['WGS84'];  //set it to something so it can at least continue
-      this.defsLoaded();
-    },
-
-/**
- * Function: loadProjCode
- *    Loads projection class code dynamically if required.
- *     Projection code may be included either through a script tag or in
- *     a built version of proj4js
- *
- */
-    loadProjCode: function(projName) {
-      if (Proj4js.Proj[projName]) {
-        this.initTransforms();
-        return;
-      }
-
-      //the URL for the projection code
-      var url = Proj4js.getScriptLocation() + 'projCode/' + projName + '.js';
-      Proj4js.loadScript(url, 
-              Proj4js.bind(this.loadProjCodeSuccess, this, projName),
-              Proj4js.bind(this.loadProjCodeFailure, this, projName), 
-              Proj4js.bind(this.checkCodeLoaded, this, projName) );
-    },
-
- /**
- * Function: loadProjCodeSuccess
- *    Loads any proj dependencies or continue on to final initialization.
- *
- */
-    loadProjCodeSuccess: function(projName) {
-      if (Proj4js.Proj[projName].dependsOn){
-        this.loadProjCode(Proj4js.Proj[projName].dependsOn);
-      } else {
-        this.initTransforms();
-      }
-    },
-
- /**
- * Function: defsFailed
- *    Report an error in loading the proj file.  Initialization of the Proj
- *    object has failed and the readyToUse flag will never be set.
- *
- */
-    loadProjCodeFailure: function(projName) {
-      Proj4js.reportError("failed to find projection file for: " + projName);
-      //TBD initialize with identity transforms so proj will still work?
-    },
-    
-/**
- * Function: checkCodeLoaded
- *    This is the loadCheck method to see if the projection code is loaded
- *
- */
-    checkCodeLoaded: function(projName) {
-      if (Proj4js.Proj[projName]) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-
 /**
  * Function: initTransforms
  *    Finalize the initialization of the Proj object
