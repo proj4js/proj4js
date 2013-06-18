fa0d8d9f8e483ff4db75c15c0d9eb2ecbb02f53c
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-06-18 10:21:24 -0400

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,21 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		concat:{
             full:{
-				src:[ './src/proj4js.js','./src/Proj.js','./src/defs.js','./src/common.js','./src/datum.js','./src/Point.js','./src/constants.js','./src/projCode/*.js','./src/defs/*.js'],
+				src:[ './src/Proj4js.js','./src/Proj.js','./src/defs.js','./src/common.js','./src/datum.js','./src/Point.js','./src/constants.js','./src/projCode/*.js','./src/defs/*.js'],
                 dest:'./dist/proj4.js'
             },
             noDefs:{
-                src:[ './src/proj4js.js','./src/Proj.js','./src/defs.js','./src/common.js','./src/datum.js','./src/Point.js','./src/constants.js','./src/projCode/*.js','./src/defs/GOOGLE.js'],
+                src:[ './src/Proj4js.js','./src/Proj.js','./src/defs.js','./src/common.js','./src/datum.js','./src/Point.js','./src/constants.js','./src/projCode/*.js','./src/defs/GOOGLE.js'],
                 dest:'./dist/proj4-noDefs.js'
             }
 		},
         uglify:{
             full:{
-    		    src:[ './src/proj4js.js','./src/Proj.js','./src/defs.js','./src/common.js','./src/datum.js','./src/Point.js','./src/constants.js','./src/projCode/*.js','./srcgrunt/defs/*.js'],
+    		    src:[ './src/Proj4js.js','./src/Proj.js','./src/defs.js','./src/common.js','./src/datum.js','./src/Point.js','./src/constants.js','./src/projCode/*.js','./srcgrunt/defs/*.js'],
                 dest:'./dist/proj4.min.js'
 		    },
             noDefs:{
-                src:[ './src/proj4js.js','./src/Proj.js','./src/defs.js','./src/common.js','./src/datum.js','./src/Point.js','./src/constants.js','./src/projCode/*.js','./src/defs/GOOGLE.js'],
+                src:[ './src/Proj4js.js','./src/Proj.js','./src/defs.js','./src/common.js','./src/datum.js','./src/Point.js','./src/constants.js','./src/projCode/*.js','./src/defs/GOOGLE.js'],
                 dest:'./dist/proj4-noDefs.min.js'
             }
         }
