6671667e97690e7d405d1a3f898fc90cf54e56da
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-06-21 12:07:27 -0400

    point is done

--- src/Point.js
+++ src/Point.js
@@ -1,63 +1,60 @@
 proj4.Point = proj4.Class({
 
-    /**
-     * Constructor: proj4.Point
-     *
-     * Parameters:
-     * - x {float} or {Array} either the first coordinates component or
-     *     the full coordinates
-     * - y {float} the second component
-     * - z {float} the third component, optional.
-     */
-    initialize : function(x,y,z) {
-      if (typeof x == 'object') {
-        this.x = x[0];
-        this.y = x[1];
-        this.z = x[2] || 0.0;
-      } else if (typeof x == 'string' && typeof y == 'undefined') {
-        var coords = x.split(',');
-        this.x = parseFloat(coords[0]);
-        this.y = parseFloat(coords[1]);
-        this.z = parseFloat(coords[2]) || 0.0;
-      } else {
-        this.x = x;
-        this.y = y;
-        this.z = z || 0.0;
-      }
-    },
-
-    /**
-     * APIMethod: clone
-     * Build a copy of a proj4.Point object.
-     *
-     * Return:
-     * {proj4}.Point the cloned point.
-     */
-    clone : function() {
-      return new proj4.Point(this.x, this.y, this.z);
-    },
-
-    /**
-     * APIMethod: toString
-     * Return a readable string version of the point
-     *
-     * Return:
-     * {String} String representation of proj4.Point object. 
-     *           (ex. <i>"x=5,y=42"</i>)
-     */
-    toString : function() {
-        return ("x=" + this.x + ",y=" + this.y);
-    },
-
-    /** 
-     * APIMethod: toShortString
-     * Return a short string version of the point.
-     *
-     * Return:
-     * {String} Shortened String representation of proj4.Point object. 
-     *         (ex. <i>"5, 42"</i>)
-     */
-    toShortString : function() {
-        return (this.x + ", " + this.y);
+  /**
+   * Constructor: proj4.Point
+   *
+   * Parameters:
+   * - x {float} or {Array} either the first coordinates component or
+   *     the full coordinates
+   * - y {float} the second component
+   * - z {float} the third component, optional.
+   */
+  initialize : function(x,y,z) {
+    if (typeof x === 'object') {
+      this.x = x[0];
+      this.y = x[1];
+      this.z = x[2] || 0.0;
+    } else if (typeof x === 'string' && typeof y === 'undefined') {
+      var coords = x.split(',');
+      this.x = parseFloat(coords[0]);
+      this.y = parseFloat(coords[1]);
+      this.z = parseFloat(coords[2]) || 0.0;
+    } else {
+      this.x = x;
+      this.y = y;
+      this.z = z || 0.0;
     }
+  },
+  /**
+   * APIMethod: clone
+   * Build a copy of a proj4.Point object.
+   *
+   * Return:
+   * {proj4}.Point the cloned point.
+   */
+  clone : function() {
+    return new proj4.Point(this.x, this.y, this.z);
+  },
+    /**
+   * APIMethod: toString
+   * Return a readable string version of the point
+   *
+   * Return:
+   * {String} String representation of proj4.Point object. 
+   *           (ex. <i>"x=5,y=42"</i>)
+   */
+  toString : function() {
+    return ("x=" + this.x + ",y=" + this.y);
+  },
+  /** 
+   * APIMethod: toShortString
+   * Return a short string version of the point.
+   *
+   * Return:
+   * {String} Shortened String representation of proj4.Point object. 
+   *         (ex. <i>"5, 42"</i>)
+   */
+  toShortString : function() {
+    return (this.x + ", " + this.y);
+  }
 });
