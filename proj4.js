aa5eb56f9d54a859233cc347946dd197b3404691
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-09-10 12:49:32 -0400

    better links in that file

--- proj4.js
+++ proj4.js
@@ -1,12 +1,11 @@
-define(['proj4/core','proj4/Proj','proj4/Point','proj4/defs','proj4/transform','proj4/mgrs'],function(proj4, Proj, Point,defs,transform,mgrs) {
-  
+define(function(require) {
+  var proj4 = require('proj4/core');
   proj4.defaultDatum = 'WGS84'; //default datum
-  proj4.Proj = Proj;
+  proj4.Proj = require('proj4/Proj');
   proj4.WGS84 = new proj4.Proj('WGS84');
-  proj4.Point = Point;
-  proj4.defs = defs;
-  proj4.transform = transform;
-  proj4.mgrs = mgrs;
+  proj4.Point = require('proj4/Point');
+  proj4.defs = require('proj4/defs');
+  proj4.transform = require('proj4/transform');
+  proj4.mgrs = require('proj4/mgrs');
   return proj4;
-
 });
