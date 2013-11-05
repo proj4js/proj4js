184197ac26e418f932cf2d1d16793df7282a3119
Calvin Metcalf <cmetcalf@appgeo.com>
2013-11-05 13:40:01 -0500

    publish script

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#you may need to run npm install -g tin
+tin -v $1
+git checkout -b build
+grunt
+git add dist/proj4.js -f
+git add dist/proj4-src.js -f
+git add package.json
+git add bower.json
+git add component.json
+git commit -m build
+git tag $1
+git push origin $version
+npm publish
+jam publish
+git checkout master
+git branch -D build
