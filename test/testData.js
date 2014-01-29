e673d1017ee54397f9594340a4f7965488a71784
Calvin Metcalf <cmetcalf@appgeo.com>
2014-01-29 08:34:41 -0500

    (#90) - upercase datums in proj strings

--- test/testData.js
+++ test/testData.js
@@ -259,6 +259,11 @@ var testPoints = [
     code:"+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371000 +b=6371000 +units=m +datum=none +no_defs",
      xy: [ 736106.55, 5893331.11 ], 
       ll: [11.0, 53.0] 
+  },
+  {
+    code:"+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs",
+    ll:[-3.20078, 55.96056],
+    xy:[325132.0089586496, 674822.638235305]
   }
 ];
 if(typeof module !== 'undefined'){
