85ee69f7be483285e8966d996654e0d2c2ff0fd7
Andreas Hocevar <andreas.hocevar@gmail.com>
2015-03-03 13:30:44 +0100

    Add handling for +units in projString

--- test/testData.js
+++ test/testData.js
@@ -285,6 +285,11 @@ var testPoints = [
     ll: [12.806988, 49.452262],
     xy: [-868208.61, -1095793.64]
   },
+  {
+    code:"+proj=tmerc +lat_0=40.5 +lon_0=-110.0833333333333 +k=0.9999375 +x_0=800000.0000101599 +y_0=99999.99998983997 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs",
+    ll: [-110.8, 43.5],
+    xy: [2434515.870, 1422072.711]
+  },
   // check that coordinates at 180 and -180 deg. longitude don't wrap around
   {
     code: 'EPSG:3857',
